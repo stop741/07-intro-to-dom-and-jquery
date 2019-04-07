841e9ed7505e01b987f2c6248221fb147562759c
stop741 <stop.741@gmail.com>
2019-04-07 18:05:39 +0300

    HW

--- starter-code/js_to_jquery_codealong/main.js
+++ starter-code/js_to_jquery_codealong/main.js
@@ -4,14 +4,29 @@
 function addToList(list, newThing) {
   var newThingLi = document.createElement('li');
   var newThingText = document.createTextNode(newThing);
+  var newThingElement = `<li>${newThing}<li>`;
   newThingLi.appendChild(newThingText);
   list.appendChild(newThingLi);
 }
 
 window.onload = function() {
-  var button = document.getElementById('new-thing-button');
+  //var button = document.getElementById('new-thing-button');
+  var $button =$('#new-thing-button'); // second method
   var thingList = document.getElementById('fav-list');
   var newThingInput = document.getElementById('new-thing');
+  var $newThingInput = ${'#newThing'}
+  $button.on('click', function(event)
+  {
+    event.preventDefault();
+    var newThing = $newThingInput.val()
+    if(newThing === '')
+    {
+      alert('You...')
+    }
+    else {
+      addToList(thingList, newThing);
+    }
+  })
 
   button.onclick = function(event) {
     event.preventDefault();
